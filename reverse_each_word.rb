77048b60fc34aa0144c0120b11cb7452cf67117f
Rick Glascock <rixong@gmail.com>
2020-01-20 05:02:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
-# def reverse_each_word(str)
-#   new_ar = []
-#   str.split(' ').each {|word| new_ar.push(word.reverse)}.join(' ')
-#   return new_ar.join(' ')
-# end
-
 def reverse_each_word(str)
-  str.split(' ').collect {|word| word.reverse}.join(' ')
+  new = []
+  str.split(' ').each { |word| new.
+    
+  }
 end
